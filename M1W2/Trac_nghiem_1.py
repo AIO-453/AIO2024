d1e552b372ba74edda504fb24b948b9518047339
AIO-453 <hntanaqc@gmail.com>
2024-06-13 12:43:51 +0700

    add thêm file sonar vô

--- M1W2/Trac_nghiem_1.py
+++ M1W2/Trac_nghiem_1.py
@@ -10,6 +10,10 @@ def max_kernel ( num_list , k ) :
         result.append(max)
     # End Code Here
     return result
+
+def devide(a/b):
+    return a/b
+
 if __name__ == '__main__':
     assert max_kernel ([3 , 4 , 5 , 1 , -44] , 3) == [5 , 5 , 5]
     num_list = [3 , 4 , 5 , 1 , -44 , 5 ,10 , 12 ,33 , 1]
